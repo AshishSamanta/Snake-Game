ccd844d76cdde325eb06fb6b8d0dea3c8659f1fc
Ashish Samanta <148355075+AshishSamanta@users.noreply.github.com>
2024-06-27 16:13:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+let inputDir={x:0,y:0};
+const foodSound= new Audio('food.mp3');
+const gameOverSound=new Audio('gameover.mp3');
+const moveSound=new Audio('move.mp3');
+const musicSound= new Audio('music.mp3');
+let lastPaintTime=0;
+let speed=5;
+let score=0;
+let sco= document.querySelector(".score");
+let X=Math.floor(19*Math.random());
+let Y=Math.floor(19*Math.random())
+let snakeArr=[
+    {x:X,y:Y}
+];
+let food={x:Math.floor(19*Math.random()),y:Math.floor(19*Math.random())};
+let board=document.querySelector("#board");
+
+const main=(ctime)=>{
+    window.requestAnimationFrame(main);
+    //console.log(ctime);
+    if((ctime-lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime=ctime;
+    gameEngine();
+}
+const isCollide=(sarr)=>{
+    //if eating itself
+    for( let i = 1 ; i < sarr.length ; i++){
+        if(sarr[i].x === sarr[0].x && sarr[i].y === sarr[0].y){
+            return true;
+        }
+    }
+    //if u hit wall
+    if( sarr[0].x >= 20 || sarr[0].x <= 0 || sarr[0].y >= 20 || sarr[0].y <= 0){
+        return true;
+    }
+    return false;
+
+
+}
+
+const gameEngine=()=>{
+    musicSound.play();
+//part1:updating the food and snake
+if(isCollide(snakeArr)){
+    gameOverSound.play();
+    musicSound.pause();
+    inputDir={x:0,y:0};
+    alert("GAME OVER!! press OK to play again");
+    snakeArr=[{x:X,y:Y}];
+    score=0;
+}
+//if u have eaten the food
+if (snakeArr[0].x===food.x && snakeArr[0].y===food.y){
+    snakeArr.unshift({x:snakeArr[0].x+inputDir.x,y:snakeArr[0].y+inputDir.y});
+    food={x:Math.floor(19*Math.random()),y:Math.floor(19*Math.random())};
+    foodSound.play();
+    score++;
+    
+}
+//moving the snake
+for(let i = snakeArr.length -2 ; i >=0  ; i--){
+    snakeArr[i+1]= {...snakeArr[i]};
+}
+snakeArr[0].x += inputDir.x;
+snakeArr[0].y += inputDir.y;
+sco.innerHTML="Score:"+ score;
+//part2:display the snake
+board.innerHTML="";
+snakeArr.forEach((e,index)=>{
+    let snakeElement = document.createElement('div');
+    snakeElement.style.gridRowStart=e.y;
+    snakeElement.style.gridColumnStart=e.x;
+    if(index===0){
+        snakeElement.classList.add('head');
+    }else{
+        snakeElement.classList.add('snake');
+    }
+    board.appendChild(snakeElement);
+});
+//dispaly food
+let foodElement = document.createElement('div');
+foodElement.style.gridRowStart=food.y;
+foodElement.style.gridColumnStart=food.x;
+foodElement.classList.add('food');
+board.appendChild(foodElement);
+}
+window.requestAnimationFrame(main);
+window.addEventListener('keydown',e=>{
+    inputDir = {x:0,y:1};
+    moveSound.play();
+    switch(e.key){
+        case "ArrowUp":
+            console.log("ArrowUp");
+            inputDir.x=0;
+            inputDir.y=-1;
+            break;
+        case "ArrowDown":
+            console.log("ArrowDown");
+            inputDir.x=0;
+            inputDir.y=1;
+            break;
+        case "ArrowLeft":
+            console.log("ArrowLeft");
+            inputDir.x=-1;
+            inputDir.y=0;
+            break;
+        case "ArrowRight":
+            console.log("ArrowRight");
+            inputDir.x=1;
+            inputDir.y=0;
+            break;
+        default:
+            break;                    
+    }
+});
